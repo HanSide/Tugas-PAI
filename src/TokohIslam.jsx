3efed7068faa4d0f3d3359cbd96da4952d3631de
HanSide <ihanaulia@gmail.com>
2025-05-18 12:27:58 +0700

    final

---
+++ src/TokohIslam.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import './index.css';
+
+
+
+
+const TokohIslam = () => {
+  return (
+    <div className="tokoh-container fade-in">
+      <div className="tokoh-header">
+      <h1 className="tokoh-title">KH. Sahal Mahfudz</h1>
+      <h3 className="tokoh-subtitle">Pati, Jawa Tengah</h3>
+      <img src="https://cdn.antaranews.com/cache/1200x800/2014/01/20140124sahal_mahfudz.jpg" alt="KH SAHAL MAHFUDZ" />
+</div>
+      <div className="tokoh-section">
+        <h4>Biografi</h4>
+        <p>
+         Nama lengkap KH. MA. Sahal Mahfudz (selanjutnya disebut dengan Kyai Sahal) adalah Muhammad Ahmad Sahal bin Mahfudz bin Abd. Salam Al-Hajaini lahir di Desa Kajen, Margoyoso Pati pada tanggal 17 Desember 1937.
+Beliau adalah anak ketiga dari enam bersaudara yang merupakan ulama kontemporer Indonesia yang disegani karena kehati-hatiannya dalam bersikap dan kedalaman ilmunya dalam memberikan fatwa terhadap masyarakat baik dalam ruang lingkup lokal (masyarakat dan pesantren yang dipimpinnya) dan ruang lingkup nasional.
+Sebelum orang mengenal Kyai Sahal, orang akan mengenalnya sebagai sosok yang biasa-biasa saja. Dengan penampilan yang sederhana orang mengira, beliau sebagai orang biasa yang tidak punya pengetahuan apapun. Namun ternyata pengetahuan dan kepakaran Kyai Sahal sudah diakui. Salah satu contoh, sosok yang menjadi pengasuh pesantren2 ini pernah bergabung dengan institusi yang bergerak dalam bidang pendidikan, yaitu menjadi anggota BPPN3 selama 2 periode yaitu dari tahun 1993-2003.
+Kyai Sahal lahir dari pasangan Kyai Mahfudz bin Abd. Salam al- Hafidz (w 1944 M) dan Hj. Badi’ah (w. 1945 M) yang sedari lahir hidup di pesantren, dibesarkan dalam lingkungan pesantren, belajar hingga ladang pengabdiannya pun ada di pesantren. Saudara Kyai Sahal yang berjumlah lima orang yaitu, M. Hasyim, Hj. Muzayyanah (istri KH. Mansyur Pengasuh PP An-Nur Lasem), Salamah (istri KH. Mawardi, pengasuh PP Bugel-Jepara, kakak istri KH. Abdullah Salam ), Hj. Fadhilah (istri KH. Rodhi Sholeh Jakarta), Hj. Khodijah (istri KH. Maddah, pengasuh PP Assuniyah Jember yang juga cucu KH. Nawawi, adik kandung KH. Abdussalam, kakek KH. Sahal.).
+Pada tahun 1968/69 Kyai Sahal menikah dengan Dra Hj Nafisah binti KH. Abdul Fatah Hasyim, Pengasuh Pesantren Fathimiyah Tambak Beras Jombang dan berputra Abdul Ghofar Rozin yang sejak sekarang sudah dipersiapkan untuk menggantikan kepemimpinan Kyai Sahal.
+        </p>
+      </div>
+
+      <div className="tokoh-section">
+        <h4>Wilayah Dakwah</h4>
+        <h5>
+          1. Pondok Pesantren Maslakul Huda – Sarang, Rembang (Jawa Tengah)
+        </h5>
+        <p>
+         Wilayah utama dakwah KH Sahal Mahfudh adalah di Pondok Pesantren Maslakul Huda, yang berlokasi di Sarang, Kabupaten Rembang, Jawa Tengah.
+Di pesantren inilah KH Sahal mengembangkan sistem pendidikan Islam berbasis kitab kuning, namun berpikiran terbuka terhadap ilmu modern dan isu sosial kemasyarakatan.
+        </p>
+        <h5>
+          2. Jawa Tengah dan Wilayah Sekitarnya
+          </h5>
+          <p>
+            Mengisi pengajian,
+
+Ceramah keagamaan,
+
+Musyawarah ulama,
+
+Konsultasi fatwa sosial.
+          </p>
+          <h5>
+            3. Wilayah Nasional – Melalui NU dan MUI
+          </h5>
+          <p>
+            Peran KH Sahal berkembang ke tingkat nasional saat beliau:
+
+Menjadi Rais ‘Aam PBNU (pemimpin tertinggi Nahdlatul Ulama)
+
+Menjadi Ketua Umum MUI (Majelis Ulama Indonesia)
+
+Lewat jabatan tersebut, beliau menjangkau masyarakat seluruh Indonesia, termasuk:
+
+Memberi fatwa nasional
+
+Menjadi rujukan umat dan pemerintah
+
+Terlibat dalam isu kebangsaan dan keagamaan tingkat nasional
+          </p>
+      </div>
+
+      <div className="tokoh-section">
+        <h4>Hasil Karya Dari KH. Sahal Mafdudz</h4>
+        <ul>
+          <h5>Hasil Karya Yang Diterbitkan</h5>
+          <p className='hasilkarya'>
+Thariqatal-Hushul ila Ghayahal-Ushul, (Surabaya: Diantarna, 2000)
+Pesantren Mencari Makna, (Jakarta: Pustaka Ciganjur, 1999)
+Al-Bayan al-Mulamma’ ‘an Alfdz al-Lumd”, (Semarang: Thoha Putra, 1999)
+Telaah Fikih Sosial, Dialog dengan KH. MA. Sahal Mahfudh, (Semarang: Suara Merdeka, 1997)
+Nuansa Fiqh Sosial (Yogyakarta: LKiS, 1994)
+Ensiklopedi Ijma’ (terjemahan bersama KH. Mustofa Bisri dari kitab Mausu’ah al-Ij ma’). (Jakarta; Pustaka Firdaus, 1987).
+Al-Tsamarah al-Hajainiyah, I960 (Nurussalam, t.t)
+Luma’ al-Hikmah ila Musalsalat al-Muhimmat, (Diktat Pesantren Maslakul Huda, Pati).
+Al-Faraid al-Ajibah, 1959 (Diktat Pesantren Maslakul Huda, Pati)"</p>
+          <h5>Hasil Karya Yang Ditidak Diterbitkan</h5>
+        
+            <p className='hasilkarya'>
+              "Tipologi Sumber Day a Manusia Jepara dalam Menghadapi AFTA 2003 (Workshop KKNINISNU Jepara, 29 Pebruari 2003).
+Strategi dan Pengembangan SDM bagi Institusi Non-Pemerintah, (Lokakarya Lakpesdam NU, Bogor, 18 April 2000).
+Mengubah Pemahaman atas Masyarakat: Meletakkan Paradigma Kebangsaan dalam Perspektif Sosial (Silarurahmi Pemda II Ulama dan Tokoh Masyarakat Purwodadi, 18 Maret 2000).
+Pokok-Pokok Pikiran tentang Militer dan Agama (Halaqah Nasional PB NU dan P3M, Malang, 18 April 2000)
+Prospek Sarjana Muslim Abad XXI, (Stadium General STAI al-Falah Assuniyah, Jember, 12 September 1998)
+Keluarga Maslahah dan Kehidupan Modern, (Seminar Sehari LKKNU, Evaluasi Kemitraan NU-BKKBN, Jakarta, 3 Juni 1998)
+Pendidikan Agama dan Pengaruhnya terhadap Penghayatan dan Pengamalan Budi Pekerti, (Sarasehan Peningkatan Moral Warga Negara Berdasarkan Pancasila BP7 Propinsi Jawa Tengah, 19 Juni 1997)
+Metode Pembinaan Aliran Sempalan dalam Islam, (Semarang, 11 Desember 1996)
+Perpustakaan dan Peningkatan SDM Menurut Visi Islam, (Seminar LP Ma’arif, Jepara, 14 Juli 1996)
+Arah Pengembangan Ekonomi dalam Upaya Pemberdayaan Ekonomi Umat, (Seminar Sehari, Jember, 27 Desember 1995)
+Pendidikan Pesantren sebagai Suatu Alternatif Pendidikan Nasional, (Seminar Nasional tentang Peranan Lembaga Pendidikan Islam dalam Peningkatan Kualitas SDM Pasca 50 tahun Indonesia Merdeka, Surabaya, 2 Juli 1995)
+Peningkatan Penyelenggaraan Ibadah Haji yang Berkualitas, (disampaikan dalam Diskusi Panel, Semarang, 27 Juni 1995)
+Pandangan Islam terhadap Wajib Belajar, (Penataran Sosialisasi Wajib belajar 9 Tahun, Semarang 10 Oktober 1994)
+Perspektif dan Prospek Madrasah Diniyah, (Surabaya, 16 Mei 1994)
+Fiqh Sosial sebagai Alternatif Pemahaman Beragama Masyarakat, (disampaikan dalam kuliah umum IKAHA, Jombang, 28 Desember 1994)
+Reorientasi Pemahaman Fiqh, Menyikapi Pergeseran Perilaku Masyarakat, (disampaikan pada Diskusi Dosen Institut Hasyim Asy’ari, Jombang, 27 Desember 1994)
+Sebuah Releksi tentang Pesantren, (Pati, 21 Agustus 1993)
+Posisi Umat Islam Indonesia dalam Era Demokratisasi dari Sudut Kajian Politis, (Forum Silaturahmi PP Jateng, Semarang, 5 September 1992).
+Kepemimpinan Politik yang Berkeadilan dalam Islam, (Halaqah Fiqh Imaniyah, Yogyakarta, 3-5 Nopember 1992)
+Peran Ulama dan Pesantren dalam Upaya Peningkatan Derajat Kesehatan Umat, (Sarasehan Opening RSU Sultan Agung, Semarang, 26 Agustus 1992).
+Pandangan Islam Terhadap AIDS, (Seminar, Surabaya,1 Desember 1992)
+Kata Pengantar dalam buku Quo Vadis NU karya Kacung Marijan, (Pati, 13 Pebruari 1992)
+Peranan Agama dalam Pembinaan Gizi dan Kesehatan Keluarga, Pandangan dari Segi Posisi Tokoh Agama, Muallim, dan Pranata Agama, (Muzakarah Nasional, Bogor, 2 Desember 1991)
+Mempersiapkan Generasi Muda Islam Potensial, (Siaran Mimbar Agama Islam TVRI, Jakarta, 24 Oktober 1991)
+Moral dan Etika dalam Pembangunan, (Seminar Kodam IV, Semarang, 18-19 September 1991)
+Pluralitas Gerakan Islam dan Tantangan Indonesia Masa Depan, Perpsketif Sosial Ekonomi, (Seminar di Yogyakarta, 10 Maret 1991)
+Islam dan Politik, (Seminar, Kendal, 4 Maret 1989)
+Filosofi dan Strategi Pengembangan Masyarakat di Lingkungan NU, (disampaikan dalam Temu Wicara LSM, Kudus, 10 September 1989)
+Disiplin dan Ketahanan Nasional, Sebuah Tinjauan dari Ajaran Islam, (Forum MUIII, Kendal, 8 Oktober 1988)
+Relevansi Ulumuddiyanah di Pesantren dan Tantangan Masyarakat, (Mudzakarah, P3M, Mranggen, 19-21 September 1988)
+Prospek Pesantren dalam Pengembangan Science, (Refreshing Course KPM, Tambak Beras, Jombang 19 Januari 1988)
+Ajaran Aswaja dan Kaitannya dengan Sistem Masyarakat, (LKL GP Anshor dan Fatayat, Jepara 12-17 Februari 1988)
+AIDS dan Prostisusi dari Dimensi Agama Islam, (Seminar AIDS dan Prostitusi YAASKI, Yogyakarta, 21 Juni 1987)
+Sumbangan Wawasan tentang Madrasah dan Ma’arif, (Raker LP Ma’arif, Pati, 21 Desember 1986)
+Program KB dan Ulama, (Pati, 27 Oktober 1986)
+Hismawati dan Taman Gizi, (Sarasehan gizi antar santriwati,
+Administrasi Pembukuan Keuangan Menurut Pandangan Islam, (Latihan Administrasi Pembukuan dan Keuangan bagi TPM, Pan, 8 April 1986)
+Pendekatan Pola Pesantren sebagai Salah Satu Alternatif membudayakan NKKBS, (Rapat Konsultasi Nasional Bidang, KB, Jakarta, 23-27 Januari 1984)
+Pelaksanaan Pendidikan Kependudukan di Pesantren, (Lokakarya Pendidikan Kependudukan di Pesantren, (Jakarta, 6-8 Januari 1983)
+Tanggapan atas Pokok-Pokok Pikiran Pembaharuan Pendidikan Nasional, (27 Nopember 1979)
+Peningkatan Sosial Amaliah Islam, (Pekan Orientasi Ulama Khotib, Pati, 21-23 Pebruari 1977)
+Intifah al-Wajadain, (Risalah tidak diterbitkan)
+Wasmah al-Sibydn ild I’tiqdd ma’ da al-Rahman, (Risalah tidak diterbitkan)
+I’dnah al-Ashhdb, 1961 (Risalah tidak diterbitkan)
+Faid al-Hija syarah Nail al-Raja dan Nazhdm Safinah al-Naja, 1961 (Risalah tidak diterbitkan)
+Al-Tarjamah al-Munbalijah ‘an Qasiidah al-Munfarijah, (Risalah tidak diterbitkan)"
+            </p>
+        </ul>
+      </div>
+
+      <div className="tokoh-section">
+        <h4>Peran dalam Perkembangan Islam</h4>
+         <h5>1. Peran dalam Penyebaran Islam Moderat</h5>
+        <p>KH Sahal aktif menyebarkan nilai-nilai Islam yang ramah, toleran, dan menjunjung tinggi perdamaian.
+
+Menolak keras paham ekstrem dan kekerasan atas nama agama.
+
+Lewat ceramah, tulisan, dan organisasi keagamaan, beliau memperkenalkan Islam yang selaras dengan nilai-nilai kemanusiaan dan kebangsaan.</p>
+      <h5>2. Peran dalam Pembaharuan Pendidikan Islam</h5>
+      <p>
+        Mengembangkan sistem pendidikan pesantren agar mampu mengikuti perkembangan zaman.
+
+Menggabungkan ilmu agama (tradisional) dengan ilmu umum dan keterampilan hidup modern.
+
+Mendorong pesantren agar tidak eksklusif, tapi adaptif terhadap dunia luar.
+      </p>
+      <h5>3. Peran dalam Menjaga dan Menguatkan Tradisi Islam Nusantara</h5>
+      <p>Menjaga amaliah NU seperti tahlilan, manaqiban, dan maulidan agar tetap lestari.
+
+Meyakini bahwa budaya lokal dapat menjadi sarana dakwah Islam yang efektif dan membumi.
+
+Menjadikan Islam terasa akrab, dekat, dan tidak mengancam bagi masyarakat awam.
+</p>
+<h5>
+  4. Peran sebagai Penengah Konflik dan Pemersatu Umat
+</h5>
+<p>
+  Menjadi tokoh yang sering diminta pendapatnya dalam menghadapi konflik sosial-keagamaan.
+
+Sering berperan sebagai penyejuk di tengah perbedaan pendapat antar kelompok Islam.
+
+Menjembatani kelompok tradisional dan modern agar tidak saling menyerang.
+</p>
+<h5>
+  5. Peran Strategis di Organisasi Keagamaan
+</h5>
+<p>
+  Menjabat sebagai Rais ‘Aam PBNU (pemimpin tertinggi Nahdlatul Ulama).
+
+Juga pernah menjadi Ketua Umum Majelis Ulama Indonesia (MUI).
+
+Lewat jabatan ini, beliau banyak memberi arah kebijakan keagamaan nasional yang sejuk dan berpihak pada rakyat.
+</p>
+
+      </div>
+    </div>
+  );
+};
+
+export default TokohIslam;
